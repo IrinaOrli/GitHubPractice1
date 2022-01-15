2a55324cd4e5717139c25b71c041c39cc22311cc
IrinaOrli <93168720+IrinaOrli@users.noreply.github.com>
2022-01-15 10:29:36 -0500

    created substract method

--- src/gitPractice/Calculator.java
+++ src/gitPractice/Calculator.java
@@ -9,4 +9,12 @@ public class Calculator {
         int result = num1 + num2;
         return result;
     }
+
+
+    public static int subtract(int num1, int num2) {
+        int result = num1 - num2;
+        return result;
+    }
+
+
 }
